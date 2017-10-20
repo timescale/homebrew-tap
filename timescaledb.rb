234648310386e85af0865a0e1dfca676605bc854
Rob Kiefer <rob@timescale.com>
2017-10-20 16:22:53 -0400

    Re-release 0.6.0 (2)

--- timescaledb.rb
+++ timescaledb.rb
@@ -2,7 +2,7 @@ class Timescaledb < Formula
   desc "An open-source time-series database optimized for fast ingest and complex queries. Fully compatible with PostgreSQL."
   homepage "https://www.timescaledb.com"
   url "https://timescalereleases.blob.core.windows.net/homebrew/timescaledb-0.6.0.tar.gz"
-  version "0.6.0-1"
+  version "0.6.0-2"
   sha256 "581c82baeb3bbd73684ff362d09a21aaae87609115b5f9aba8d3b098a34bd9ba"
 
   depends_on "postgresql@9.6" => :build
@@ -10,18 +10,32 @@ class Timescaledb < Formula
   def install
     system "make"
     system "make", "install", "DESTDIR=#{buildpath}/stage"
-    (lib/"timescaledb").install "timescaledb.so"
-  end
+    libdir = `pg_config --libdir`
+    sharedir = `pg_config --sharedir`
+    `touch timescaledb_move.sh`
+    `chmod +x timescaledb_move.sh`
+    `echo "#!/bin/bash" >> timescaledb_move.sh`
+    `echo "echo 'Moving files into place...'" >> timescaledb_move.sh`
+    `echo "/usr/bin/install -c -m 755 #{lib}/timescaledb/timescaledb.so #{libdir.strip}/timescaledb.so" >> timescaledb_move.sh`
+    `echo "/usr/bin/install -c -m 644 #{share}/timescaledb/* #{sharedir.strip}/extension/" >> timescaledb_move.sh`
+    `echo "echo 'Success.'" >> timescaledb_move.sh`
+    bin.install "timescaledb_move.sh"
+    (lib/"timescaledb").install Dir["stage/**/lib/*"]
+    (share/"timescaledb").install Dir["stage/**/share/postgresql@9.6/extension/*"]
+    end
 
   test do
-    pglib = (lib/"postgresql")
-    system "test", "-e", "#{pglib}/timescaledb.so"
+    system "test", "-e", "#{lib}/timescaledb/timescaledb.so"
   end
 
   def caveats
-    pgvar = (var/"postgres")
+    pgvar = (var/"postgresql@9.6")
     s = "Make sure to update #{pgvar}/postgresql.conf to include the extension:\n\n"
-    s += "shared_preload_libraries = 'timescaledb'\n\n"
+    s += "  shared_preload_libraries = 'timescaledb'\n\n"
+    s += "To finish the installation, you will need to run:\n\n"
+    s += "  $ timescaledb_move.sh\n\n"
+    s += "This will install the extension files in the proper place. Finally, restart:\n\n"
+    s += "  $ brew services restart postgresql@9.6\n\n"
     s
   end
 end
