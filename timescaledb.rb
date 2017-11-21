7ea1ac1169a80f7197ab1c1bba78463e8fb22254
Rob Kiefer <rob@timescale.com>
2017-11-21 16:19:30 -0500

    Release 0.7.0

--- timescaledb.rb
+++ timescaledb.rb
@@ -1,27 +1,29 @@
 class Timescaledb < Formula
   desc "An open-source time-series database optimized for fast ingest and complex queries. Fully compatible with PostgreSQL."
   homepage "https://www.timescaledb.com"
-  url "https://timescalereleases.blob.core.windows.net/homebrew/timescaledb-0.6.1.tar.gz"
-  version "0.6.1"
-  sha256 "5d570c3931eb6363c07a9f7bc0b15cf75b26ea9890b5681c19790b719fe4a211"
+  url "https://timescalereleases.blob.core.windows.net/homebrew/timescaledb-0.7.0.tar.gz"
+  version "0.7.0"
+  sha256 "475ab2e2b596e3048c2c5b4e7d698855d23cdf970c415ac8794d5beabf6b29ed"
 
-  depends_on "postgresql@9.6" => :build
+  depends_on "cmake" => :build
+  depends_on :postgresql => :build
 
   def install
-    system "make"
-    system "make", "install", "DESTDIR=#{buildpath}/stage"
-    libdir = `pg_config --libdir`
+    system "./bootstrap"
+    system "cd ./build && make"
+    system "cd ./build && make install DESTDIR=#{buildpath}/stage"
+    libdir = `pg_config --pkglibdir`
     sharedir = `pg_config --sharedir`
     `touch timescaledb_move.sh`
     `chmod +x timescaledb_move.sh`
     `echo "#!/bin/bash" >> timescaledb_move.sh`
     `echo "echo 'Moving files into place...'" >> timescaledb_move.sh`
-    `echo "/usr/bin/install -c -m 755 #{lib}/timescaledb/timescaledb.so #{libdir.strip}/timescaledb.so" >> timescaledb_move.sh`
+    `echo "/usr/bin/install -c -m 755 \\\$(find #{lib} -name timescaledb.so) #{libdir.strip}/timescaledb.so" >> timescaledb_move.sh`
     `echo "/usr/bin/install -c -m 644 #{share}/timescaledb/* #{sharedir.strip}/extension/" >> timescaledb_move.sh`
     `echo "echo 'Success.'" >> timescaledb_move.sh`
     bin.install "timescaledb_move.sh"
     (lib/"timescaledb").install Dir["stage/**/lib/*"]
-    (share/"timescaledb").install Dir["stage/**/share/postgresql@9.6/extension/*"]
+    (share/"timescaledb").install Dir["stage/**/share/postgresql*/extension/*"]
     end
 
   test do
@@ -29,13 +31,14 @@ class Timescaledb < Formula
   end
 
   def caveats
-    pgvar = (var/"postgresql@9.6")
-    s = "Make sure to update #{pgvar}/postgresql.conf to include the extension:\n\n"
+    pgvar = `find /usr/local/var/postgres* -name "postgresql.conf" | head -n 1`
+    s = "Make sure to update #{pgvar.strip} to include the extension:\n\n"
     s += "  shared_preload_libraries = 'timescaledb'\n\n"
     s += "To finish the installation, you will need to run:\n\n"
     s += "  $ timescaledb_move.sh\n\n"
-    s += "This will install the extension files in the proper place. Finally, restart:\n\n"
-    s += "  $ brew services restart postgresql@9.6\n\n"
+    s += "This will install the extension files in the proper place. \n"
+    s += "If installed via Homebrew:\n\n"
+    s += "  $ brew services restart postgresql\n\n"
     s
   end
 end
