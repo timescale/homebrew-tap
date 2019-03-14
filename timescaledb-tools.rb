c659ace790e8435075008b72cf7ac353e38bc318
Rob Kiefer <rob@timescale.com>
2019-03-14 11:19:31 -0400

    Release timescaledb-tools 0.5.0

--- timescaledb-tools.rb
+++ timescaledb-tools.rb
@@ -1,9 +1,9 @@
 class TimescaledbTools < Formula
   desc "Client tools for working with TimescaleDB"
   homepage "https://www.timescaledb.com"
-  url "https://timescalereleases.blob.core.windows.net/homebrew/timescaledb-tools-0.4.1.tar.gz"
-  version "0.4.1"
-  sha256 "2d166d1e9750df5b91ef06d3c14c1cc1693db16fd3b805a88432254d9e3ad44a"
+  url "https://timescalereleases.blob.core.windows.net/homebrew/timescaledb-tools-0.5.0.tar.gz"
+  version "0.5.0"
+  sha256 "933efb68b208091cac286258bccd5aa0065bd2ddc22d93dda72f5debf5bd8b6f"
 
   def install
     bin.install "timescaledb-tune"
