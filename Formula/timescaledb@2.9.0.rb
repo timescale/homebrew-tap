966ed564217c6da0f5356127ee0cbe534ba9fe34
Ryan Walker <rwalker@timescale.com>
2022-10-19 17:04:40 -0400

    offer versioned formulae

---
+++ Formula/timescaledb@2.9.0.rb
@@ -0,0 +1,6 @@
+require_relative "../timescaledb_base"
+
+class TimescaledbAT290 < TimescaledbBase
+  sha256 "666f5427cb036500bca731d24d845285299cff82f576ad4474f85da682b2ee78"
+  url "https://github.com/timescale/timescaledb/archive/refs/tags/2.9.0.tar.gz"
+end
---
+++ Formula/timescaledb@2.9.1.rb
@@ -0,0 +1,6 @@
+require_relative "../timescaledb_base"
+
+class TimescaledbAT291 < TimescaledbBase
+  sha256 "883638f2e79d25ec88ee58f603f3c81c999b6364cb4c799919d363f04089b47b"
+  url "https://github.com/timescale/timescaledb/archive/refs/tags/2.9.1.tar.gz"
+end
