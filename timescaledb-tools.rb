7c9fe50c1e68ddbab93506832f071b32a5b0e093
Sven Klemm <31455525+svenklemm@users.noreply.github.com>
2022-10-24 15:05:28 +0200

    Fix version in timescaledb-tools

--- timescaledb-tools.rb
+++ timescaledb-tools.rb
@@ -2,7 +2,7 @@ class TimescaledbTools < Formula
   desc "Client tools for working with TimescaleDB"
   homepage "https://www.timescaledb.com"
   url "https://github.com/timescale/timescaledb-tune/releases/download/v0.14.1/timescaledb-tools-0.14.1-darwin.tgz"
-  version "0.13.0"
+  version "0.14.1"
   sha256 "300d760062408f767ef2b020658578fc9d5b405522d8d4d78ddf8623cae5469c"
 
   def install
