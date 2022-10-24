8d7b499ce8cc66b5385ac3169f9f071cfc8fb677
Sven Klemm <31455525+svenklemm@users.noreply.github.com>
2022-10-24 18:27:40 +0200

    Bump tools version to 0.14.2.

--- timescaledb-tools.rb
+++ timescaledb-tools.rb
@@ -1,9 +1,9 @@
 class TimescaledbTools < Formula
   desc "Client tools for working with TimescaleDB"
   homepage "https://www.timescaledb.com"
-  url "https://github.com/timescale/timescaledb-tune/releases/download/v0.14.1/timescaledb-tools-0.14.1-darwin.tgz"
-  version "0.14.1"
-  sha256 "300d760062408f767ef2b020658578fc9d5b405522d8d4d78ddf8623cae5469c"
+  url "https://github.com/timescale/timescaledb-tune/releases/download/v0.14.2/timescaledb-tools-0.14.2-darwin.tgz"
+  version "0.14.2"
+  sha256 "f8ad6ae64814c313e1510c91f611dc35fb375456676b3dbe2e8f8ae32597c79d"
 
   def install
     bin.install "timescaledb-tune"
