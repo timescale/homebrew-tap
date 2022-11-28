1f29687d3ed2493d61a7d539f40eb0f1fcf177a8
Ryan Walker <rwalker@timescale.com>
2022-11-28 16:05:52 -0500

    create Aliases subdir and link latest timescaledb

--- Formula/timescaledb.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'timescaledb@2.8.1'
-
-# This is the class that contains a link to the latest version of TimescaleDB
-# Only inherits the latest version and require the proper relative.
-# Feed the TimescaleDB::VERSION_MAP to keep it up to date.
-class Timescaledb < TimescaledbAT281
-end
