2d5e96ce6f06b5c9033b92424272306bc76f742a
Konstantina Skovola <44239391+konskov@users.noreply.github.com>
2022-12-21 10:50:15 +0200

    Update timescaledb-tools.rb
    Update for timescaledb-tune version 0.14.3
    
    Signed-off-by: Konstantina Skovola <44239391+konskov@users.noreply.github.com>

--- timescaledb-tools.rb
+++ timescaledb-tools.rb
@@ -1,9 +1,9 @@
 class TimescaledbTools < Formula
   desc "Client tools for working with TimescaleDB"
   homepage "https://www.timescaledb.com"
-  url "https://github.com/timescale/timescaledb-tune/releases/download/v0.14.2/timescaledb-tools-0.14.2-darwin.tgz"
-  version "0.14.2"
-  sha256 "f8ad6ae64814c313e1510c91f611dc35fb375456676b3dbe2e8f8ae32597c79d"
+  url "https://github.com/timescale/timescaledb-tune/releases/download/v0.14.3/timescaledb-tools-0.14.3-darwin.tgz"
+  version "0.14.3"
+  sha256 "e3f546f2c7bfce82ad008a7af3ad05940a46266e6fe2006fcb82a60c3eb1ed20"
 
   def install
     bin.install "timescaledb-tune"
