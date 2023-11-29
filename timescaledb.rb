645c12c65b7789683e4e8acf9f2289373ed83bfe
CI <ci@timescale.com>
2023-11-29 09:17:21 +0000

    Update timescaledb homebrew

--- timescaledb.rb
+++ timescaledb.rb
@@ -1,9 +1,9 @@
 class Timescaledb < Formula
   desc "An open-source time-series database optimized for fast ingest and complex queries. Fully compatible with PostgreSQL."
   homepage "https://www.timescaledb.com"
-  url "https://github.com/timescale/timescaledb/archive/refs/tags/2.12.2.tar.gz"
-  sha256 "d24cd29aad411d3e41d543d1a30827c48f8e01a48563650db5fac145ad7a01eb"
-  version "2.12.2"
+  url "https://github.com/timescale/timescaledb/archive/refs/tags/2.13.0.tar.gz"
+  sha256 "584a351c7775f0e067eaa0e7277ea88cab9077cc4c455cbbf09a5d9723dce95d"
+  version "2.13.0"
   env :std
 
   option "with-oss-only", "Build TimescaleDB with only Apache-2 licensed code"
