333f36c5d05c07c393d7fae4af6bbeb501baf6e5
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-11-26 15:54:38 -0300

    Update postgresql dependency from pg16 to pg17

--- timescaledb.rb
+++ timescaledb.rb
@@ -10,12 +10,12 @@ class Timescaledb < Formula
 
   depends_on "cmake" => :build
   depends_on "openssl" => :build
-  depends_on "postgresql@16" => :build
+  depends_on "postgresql@17" => :build
   depends_on "xz" => :build
   depends_on "timescale/tap/timescaledb-tools" => :recommended
 
   def postgresql
-    Formula["postgresql@16"]
+    Formula["postgresql@17"]
   end
 
   def install
